c25643d68debed94c6aac6272e0538d9ea44dcc5
Ben Do <bdo.appworkshop@gmail.com>
2023-01-18 11:44:31 -0600

    Removed footer.

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
-      <footer>
-        <p>2022 Ben Do</p>
-        <p><a href="mailto:hege@example.com">hege@example.com</a></p>
-      </footer>
     </div>
   )
 }
