4ad9d22268e3aa06993c389faf4ba05e2f4ff49e
Ben Do <bdo.appworkshop@gmail.com>
2023-01-20 12:13:36 -0600

    Implemented getStaticProps, added card layout for coffee stores.

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,25 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner';
+import Card from '../components/card';
+import coffeeStoresData from '../data/coffee-stores';
+import {fetchCoffeeStores} from '../lib/coffee-stores';
 
-export default function Home() {
+export async function getStaticProps(context) {
+
+  const coffeeStores = await fetchCoffeeStores();
+  console.log('context', context);
+
+  return {
+    props: {
+      coffeeStores,
+    }, // will be passed to the page component as props
+  }
+}
+
+export default function Home(props) {
+
+
   const handleOnBannerBtnClick = () => {
     console.log('Hi banner button');
   };
@@ -20,6 +37,24 @@ export default function Home() {
         <Banner buttonText='View stores nearby' handleOnClick={handleOnBannerBtnClick} />
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400} alt='banner' />
+        </div>
+        <div className={styles.cardLayout}>
+          {props.coffeeStores.length > 0 && (
+            <>
+              <h2 className={styles.heading2}>Chicago Stores</h2>
+              {props.coffeeStores.map(coffeeStore => {
+                return (
+                  <Card
+                    key={coffeeStore.fsq_id}
+                    name={coffeeStore.name}
+                    imgUrl={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'}
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                )
+              })}
+            </>
+          )}
         </div>
       </main>
     </div>
